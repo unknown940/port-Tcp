0d51b72b20ac95c895bcb51a1d163a129773c49d
Black Team <79963263+unknown940@users.noreply.github.com>
2021-03-03 09:51:48 +0300

    Add files via upload

---
+++ unknownscan.py
@@ -0,0 +1,26 @@
+import socket
+print('''
+
+ ____               __    ______                  
+/\  _`\            /\ \__/\__  _\                 
+\ \ \L\ \___   _ __\ \ ,_\/_/\ \/   ___   _____   
+ \ \ ,__/ __`\/\`'__\ \ \/  \ \ \  /'___\/\ '__`\    
+  \ \ \/\ \L\ \ \ \/ \ \ \_  \ \ \/\ \__/\ \ \L\ \     
+   \ \_\ \____/\ \_\  \ \__\  \ \_\ \____\\ \ ,__/    
+    \/_/\/___/  \/_/   \/__/   \/_/\/____/ \ \ \/     
+                                            \ \_\    
+                                             \/_/    
+''')
+
+
+
+host=input('Enter Ip Target\n===>')
+for port in range (0,250):
+ a=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ a.settimeout(0.1)
+ b=a.connect_ex((host, port))
+ if b == 0:
+     print(port, 'is open')
+     break
+ else:
+     print(port, 'is closed')
